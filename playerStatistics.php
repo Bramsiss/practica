bd0ec39e47102ea5f414f7d7ea81a2ac8e40b8fe
Bramsiss <bramsiss@yandex.ru>
2016-02-21 02:19:32 +0400

    Added files via upload

--- playerStatistics.php
+++ playerStatistics.php
@@ -4,8 +4,8 @@
 
     //Запрос детальных статистических данных об игроке
     
-    champ
-    season
+    private $_champ;
+    private $_season;
     private $_club;
     private $_games;
     private $_subs;
@@ -15,68 +15,86 @@
     private $_red;
     private $_voteAvg;
 
+
+    public function getChamp()
+    {
+        return $this->_champ;
+    }
+    public function getSeason()
+    {
+        return $this->_season;
+    }
     public function getClub()
     {
         return $this->_club;
     }
-        public function getGames()
+    public function getGames()
     {
         return $this->_games;
     }
-        public function getSubs()
+    public function getSubs()
     {
         return $this->_subs;
     }
-        public function getGoals()
+    public function getGoals()
     {
         return $this->_goals;
     }
-        public function getAssists()
+    public function getAssists()
     {
         return $this->_assists;
     }
-        public function getYellow()
+    public function getYellow()
     {
         return $this->_yellow;
     }
-        public function getRed()
+    public function getRed()
     {
         return $this->_red;
     }
-        public function getVoteAvg()
+    public function getVoteAvg()
     {
         return $this->_voteAvg;
     }
 
-        public function setClub($club)
+
+    public function setChamp($champ)
+    {
+        $this->_champ = $champ;
+    }
+    public function setSeason($season)
+    {
+        $this->_season = $season;
+    }
+    public function setClub($club)
     {
         $this->_club = $club;
     }
-        public function setGames($games)
+    public function setGames($games)
     {
         $this->_games = $games;
     }
-        public function setSubs($subs)
+    public function setSubs($subs)
     {
         $this->_subs = $subs;
     }
-        public function setGoals($goals)
+    public function setGoals($goals)
     {
         $this->_goals = $goals;
     }
-        public function setAssists($assists)
+    public function setAssists($assists)
     {
         $this->_assists = $assists;
     }
-        public function setYellow($yellow)
+    public function setYellow($yellow)
     {
         $this->_yellow = $yellow;
     }
-        public function setRed($red)
+    public function setRed($red)
     {
         $this->_red = $red;
     }
-        public function setVoteAvg($voteAvg)
+    public function setVoteAvg($voteAvg)
     {
         $this->_voteAvg = $voteAvg;
     }
